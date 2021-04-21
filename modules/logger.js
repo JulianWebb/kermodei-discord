a412598bc40db79af8f01f58a03d1ab43448e1c6
Julian Webb <git@julianwebb.ca>
2021-04-20 19:33:44 -0700

    Added Webhook support to logger

--- modules/logger.js
+++ modules/logger.js
@@ -1,12 +1,28 @@
+const axios = require('axios');
+const Discord = require("discord.js");
+
 module.exports = class {
     constructor(configuration) {
         this.configuration = {};
         this.configuration.enabled = configuration.enabled ?? true;
         this.configuration.name = configuration.name ?? "LOGGER";
+        this.configuration.image = configuration.image ?? "";
         this.configuration.timestamp = configuration.timestamp ?? {
             enabled: true,
             iso8601: true
         };
+        this.configuration.webhook = {
+            enabled: configuration.webhook ?? false,
+            id: process.env.WEBHOOK_ID,
+            token: process.env.WEBHOOK_TOKEN
+        }
+
+        if (this.configuration.webhook.enabled) {
+            if (!this.configuration.webhook.id || !this.configuration.webhook.token) {
+                this.error("Missing Webhook Configuration");
+            }
+            this.webhookClient = new Discord.WebhookClient(this.configuration.webhook.id, this.configuration.webhook.token);
+        }
     }
 
     get timestamp() {
@@ -22,14 +38,32 @@ module.exports = class {
         return `[${this.configuration.name}]`;
     }
 
+    webhookPrint(message) {
+        if (this.configuration.webhook.enabled) {
+            if (typeof message == "string") {
+                return this.webhookClient.send(message);
+            }
+            if (Array.isArray(message)) {
+                return this.webhookClient.send(message.reduce((accumulator, current) => accumulator + " " + current, ""), {
+                    username: this.configuration.name,
+                    avatarUrl: this.configuration.image,
+                    split: true
+                })
+            }
+        }
+    }
+
     print(prefix, message) {
         if (this.configuration.enabled) {
             if (Array.isArray(message)) {
                 message.unshift(prefix);
                 console.log.apply(this, message);
+                this.webhookPrint(message);
+                return;
             }
 
             console.log.apply(this, [prefix, message]);
+            this.webhookPrint([prefix, message]);
         }
     }
 
